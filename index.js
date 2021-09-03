da6ce617b96c04e3e76d227917d0da73e590f3ac
Karolina <kmieczkowska75@gmail.com>
2021-09-03 19:29:33 +0100

    Create change form

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const main = document.querySelector('.main');
-const btnQuantityPlus = document.querySelector('.btn__quantity--plus');
-const quantityInput = document.querySelector('.quantity__input');
-const btnQuantityMinus = document.querySelector('.btn__quantity--minus');
-const priceOutput = document.querySelector('.manage__price');
+const btnQuantityPlus = document.querySelectorAll('.btn__quantity--plus');
+const quantityInput = document.querySelectorAll('.quantity__input');
+const quantityInputChoose = document.querySelector('.quantity__input--choose');
+const quantityInputChange = document.querySelector('.quantity__input--change');
+const btnQuantityMinus = document.querySelectorAll('.btn__quantity--minus');
+const priceOutput = document.querySelectorAll('.manage__price');
+const priceOutputChoose = document.querySelector('.manage__price--choose');
+const priceOutputChange = document.querySelector('.manage__price--change');
 const btnPriceValue = document.querySelector('.btn__price');
 const btnAddToOrder = document.querySelector('.manage__btn--add');
+const btnChange = document.querySelector('.manage__btn--change');
 // const manage = document.querySelector('.manage');
 const manageChoose = document.querySelector('.manage__choose');
 const manageOrder = document.querySelector('.manage__order');
-const productTitle = document.querySelector('.manage__product');
+const manageChange = document.querySelector('.manage__change');
+const productTitle = document.querySelector('.manage__product--choose');
+const productTitleChange = document.querySelector('.manage__product--change')
 const backArrow = document.querySelectorAll('.manage__icon--back');
+const changeBackArrow = document.querySelector('.manage_icon--back-change');
 const body = document.querySelector('body');
 const footer = document.querySelector('footer');
 
@@ -119,6 +127,24 @@ const openManageOrderSection = function() {
     footer.style.display = 'none';
 }
 
+const openManageChangeSection = function() {
+
+    manageChange.style.top = '0';
+
+    manageChange.style.display = 'flex';
+
+    main.style.display = 'none';
+
+    footer.style.display = 'none';
+}
+
+const closeManageChangeSection = function() {
+    
+    manageChange.style.top = `${bodyHeight}px`;
+
+    manageChange.style.display = 'none';
+}
+
 closeManageChooseSection();
 
 // Open for styling
@@ -141,16 +167,16 @@ const displayChooseSection = function(product) {
 
     productTitle.textContent = product.title;
     
-    priceOutput.innerHTML = `£${product.price.toFixed(2)}`;
+    priceOutputChoose.innerHTML = `£${product.price.toFixed(2)}`;
 
-    priceOutput.textContent = `£${product.price.toFixed(2)}`;
+    priceOutputChoose.textContent = `£${product.price.toFixed(2)}`;
 
     btnPriceValue.textContent = `£${product.price.toFixed(2)}`;
 
-    productPrice = quantityInput.value * selectedProduct.price;
-    productQuantity = Number(quantityInput.value);
+    productPrice = quantityInputChoose.value * selectedProduct.price;
+    productQuantity = Number(quantityInputChoose.value);
 
-    priceOutput.textContent = `£${productPrice.toFixed(2)}`;
+    priceOutputChoose.textContent = `£${productPrice.toFixed(2)}`;
 
     btnPriceValue.textContent = `£${productPrice.toFixed(2)}`;
 }
@@ -167,9 +193,15 @@ productSections.forEach(function(section, ind) {
         
         selectedProduct = products[ind];
 
-        quantityInput.value = 1;
+        quantityInput.forEach(function(input) {
+            input.value = 1;
+        })
 
-        btnQuantityMinus.classList.add('state--inactive');
+        // quantityInputChoose.value = 1;
+
+        btnQuantityMinus.forEach(function(minusButton) {
+            minusButton.classList.add('state--inactive');
+        })
 
         displayChooseSection(selectedProduct);
     })
@@ -177,41 +209,62 @@ productSections.forEach(function(section, ind) {
 
 // STEP UP BUTTON
 
-btnQuantityPlus.addEventListener('click', function(ev) {
-    ev.preventDefault();
+btnQuantityPlus.forEach(function(plusButton) {
+    
+    plusButton.addEventListener('click', function(ev) {
+        ev.preventDefault();
 
-    btnQuantityMinus.classList.remove('state--inactive');
-    quantityInput.value++;
+        btnQuantityMinus.forEach(function(minusButton) {
+            minusButton.classList.remove('state--inactive');
+        })
 
-    productPrice = quantityInput.value * selectedProduct.price;
-    productQuantity = Number(quantityInput.value);
-
-    priceOutput.textContent = `£${productPrice.toFixed(2)}`;
-
-    btnPriceValue.textContent = `£${productPrice.toFixed(2)}`;
+        quantityInput.forEach(function(input) {
+            input.value++;
+            productPrice = input.value * selectedProduct.price;
+            productQuantity = Number(input.value);
+        })
+        
+        // quantityInput.value++;
+    
+        // productPrice = quantityInput.value * selectedProduct.price;
+        // productQuantity = Number(quantityInput.value);
+    
+        priceOutput.forEach(function(output) {
+            output.textContent = `£${productPrice.toFixed(2)}`;
+        })
+    
+        btnPriceValue.textContent = `£${productPrice.toFixed(2)}`;
+    })
 })
 
 // STEP DOWN BUTTON 
 
-btnQuantityMinus.addEventListener('click', function(ev) {
-    ev.preventDefault();
-
-    if (quantityInput.value > 1) {
-
-        quantityInput.value--;
-    } 
+btnQuantityMinus.forEach(function(minusButton) {
     
-    if (quantityInput.value < 2) {
-
-        this.classList.add('state--inactive');
-    }
-
-    productPrice = quantityInput.value * selectedProduct.price;
-    productQuantity = Number(quantityInput.value);
-
-    priceOutput.textContent = `£${productPrice.toFixed(2)}`;
-
-    btnPriceValue.textContent = `£${productPrice.toFixed(2)}`;
+    minusButton.addEventListener('click', function(ev) {
+        ev.preventDefault();
+    
+        quantityInput.forEach(function(input) {
+            if (input.value > 1) {
+    
+                input.value--;
+            } 
+            
+            if (input.value < 2) {
+        
+                minusButton.classList.add('state--inactive');
+            }
+    
+            productPrice = input.value * selectedProduct.price;
+            productQuantity = Number(input.value);
+        })
+    
+        priceOutput.forEach(function(output) {
+            output.textContent = `£${productPrice.toFixed(2)}`;
+        })
+    
+        btnPriceValue.textContent = `£${productPrice.toFixed(2)}`;
+    })
 })
 
 // CLOSE MANAGE SECTION
@@ -221,6 +274,8 @@ backArrow.forEach(function(arrow) {
     arrow.addEventListener('click', closeManageChooseSection);
     arrow.addEventListener('click', closeManageOrderSection);
 })
+
+changeBackArrow.addEventListener('click', closeManageChangeSection);
 
 // ADD TO ORDER
 
@@ -338,6 +393,8 @@ const yourOrders = [];
 
 // displayOrders(yourOrders);
 
+// let selectedOrder;
+
 const displayOrders = function(orders) {
     const mappedOrders = orders.map(function(order) {
         return `
@@ -352,7 +409,23 @@ const displayOrders = function(orders) {
     ordersSection.innerHTML = '';
     
     ordersSection.insertAdjacentHTML('afterbegin', mappedOrders);
-}
+
+    const orderRow = document.querySelectorAll('.order__row');
+
+    orderRow.forEach(function(row, ind) {
+        
+        row.addEventListener('click', function() {
+            displayChangeSection(ind);
+
+            btnQuantityMinus.forEach(function(minusButton) {
+                if (minusButton.classList.contains('state--inactive') && quantityInputChange.value > 1) {
+                    minusButton.classList.remove('state--inactive')
+                }
+            })
+        });
+            
+    });
+};
 
 // GO TO CHECKOUT
 
@@ -364,6 +437,41 @@ checkoutReference.addEventListener('click', function() {
 })
 
 // CHANGE QUANTITY OR REMOVE
+
+let changedOrder;
+
+const displayChangeSection = function(ind) {
+    
+    openManageChangeSection(ind);
+
+    selectedProduct.price = yourOrders[ind].selectedProductPrice / yourOrders[ind].selectedProductQuantity;
+    console.log(ind)
+    console.log(yourOrders[ind].selectedProductTitle)
+    console.log(yourOrders[ind].selectedProductPrice)
+    console.log(yourOrders[ind].selectedProductQuantity)
+    console.log(selectedProduct.price)
+
+    productTitleChange.textContent = yourOrders[ind].selectedProductTitle;
+    priceOutputChange.textContent =`£${yourOrders[ind].selectedProductPrice}`;
+    quantityInputChange.value = yourOrders[ind].selectedProductQuantity;
+
+    btnQuantityMinus.forEach(function(minusButton) {
+        if (quantityInputChange.value < 2) {
+            minusButton.classList.add('state--inactive');
+        }
+    })
+
+    changedOrder = yourOrders[ind];
+};
+
+btnChange.addEventListener('click', function(ev) {
+    ev.preventDefault();
+
+    closeManageChangeSection();
+
+    // tu skończyłam (update Order form with provided changes)
+    console.log(changedOrder)
+})
 
 
 
